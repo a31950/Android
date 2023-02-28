015d29a8aafdf7c895c24b815c8ce7bca0e95234
박헌민 <a31950@gmail.com>
2023-02-28 12:50:43 +0900

    20023-03-28 안드로이드 4교시끝!

--- proapplication1/src/main/java/com/example/proapplication1/MainActivity.kt
+++ proapplication1/src/main/java/com/example/proapplication1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.proapplication1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proapplication1.databinding.ActivityMain3Binding
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main2)
+//    }
+    private var mBinding: ActivityMain3Binding? = null
+    private val binding get() = mBinding!!
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvMessage.setText("안녕하세요 박헌민입니다.")
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
     }
 }

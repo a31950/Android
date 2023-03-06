947e6ca0ad70e6751b0d6517bf7f76c08a093cb0
박헌민 <a31950@gmail.com>
2023-03-06 10:16:13 +0900

    20023-03-06 안드로이드 1교시끝!

--- myapplication/src/main/java/com/example/myapplication/MainActivity1.kt
+++ myapplication/src/main/java/com/example/myapplication/MainActivity1.kt
@@ -1,16 +1,36 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.MotionEvent
+import android.util.Log
+import android.view.KeyEvent
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMain1Binding
 
 class MainActivity1 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main1)
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("park","onKeyUp")
+        when(keyCode){
+            KeyEvent.KEYCODE_A -> Log.d("park","A키를 눌렀네요")
+        }
+        return super.onKeyUp(keyCode, event)
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return super.onTouchEvent(event)
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("park","onKeyDown")
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMain1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.checkbox7.setOnCheckedChangeListener(object:CompoundButton.OnCheckedChangeListener{
+            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean){
+                Log.d("park","체크박스 클릭")
+        }
+    })
+
     }
 }
